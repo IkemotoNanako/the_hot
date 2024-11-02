eedc411014ed0feb1f9c0c734767a7a69f96f44b
miyasic <kou.sepak@gmail.com>
2024-11-02 22:16:32 +0900

    一応コントローラを返すように変更

--- lib/ui/page/game/hot_game.dart
+++ lib/ui/page/game/hot_game.dart
@@ -1,10 +1,8 @@
 import 'package:flame/game.dart';
 import 'package:flame_riverpod/flame_riverpod.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hackathon_2024/application/provider/usecase_providers.dart';
 import 'package:flutter_hackathon_2024/ui/controller/answers_controller.dart';
 import 'package:flutter_hackathon_2024/ui/page/game/sample_component.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class HotGame extends FlameGame with RiverpodGameMixin {
   @override
@@ -14,7 +12,7 @@ class HotGame extends FlameGame with RiverpodGameMixin {
   Future<void> onLoad() async {
     await super.onLoad();
     add(SampleComponent());
-    final snap = ref.read(listenAnswersUsecaseProvider).listenAnswers();
+    final snap = ref.read(answersControllerProvider);
     snap.listen((event) {
       print('event');
       add(SampleComponent());
--- lib/ui/page/top_page.dart
+++ lib/ui/page/top_page.dart
@@ -1,6 +1,5 @@
 import 'package:flame_riverpod/flame_riverpod.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hackathon_2024/ui/controller/answers_controller.dart';
 import 'package:flutter_hackathon_2024/ui/page/game/hot_game.dart';
 import 'package:flutter_hackathon_2024/ui/router/router.dart';
 import 'package:go_router/go_router.dart';
@@ -20,7 +19,6 @@ class TopPage extends ConsumerWidget {
     return Scaffold(
       body: Stack(
         children: [
-          const _AnswerList(),
           const _Game(),
           Center(
               child: OutlinedButton(
@@ -45,36 +43,6 @@ class _Game extends ConsumerWidget {
     return RiverpodAwareGameWidget(
       game: HotGame(),
       key: gameWidgetKey,
-    );
-  }
-}
-
-class _AnswerList extends ConsumerWidget {
-  const _AnswerList();
-
-  @override
-  Widget build(
-    BuildContext context,
-    WidgetRef ref,
-  ) {
-    return ref.watch(answersControllerProvider).when(
-      data: (answers) {
-        return ListView.builder(
-          itemCount: answers.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(answers[index].id.toString()),
-              subtitle: Text(answers[index].hotItem.id.toString()),
-            );
-          },
-        );
-      },
-      loading: () {
-        return const SizedBox.shrink();
-      },
-      error: (error, stackTrace) {
-        return const SizedBox.shrink();
-      },
     );
   }
 }
