7aec07df2d1d62e94a316fa61a30532430159ae5
miyasic <kou.sepak@gmail.com>
2024-11-02 23:25:22 +0900

    1秒ごとに落ちてくるように変更

--- lib/ui/controller/answers_controller.dart
+++ lib/ui/controller/answers_controller.dart
@@ -1,7 +1,20 @@
-import 'package:flutter_hackathon_2024/application/provider/usecase_providers.dart';
 import 'package:flutter_hackathon_2024/domain/answer.dart';
+import 'package:flutter_hackathon_2024/domain/hot_item.dart';
+import 'package:flutter_hackathon_2024/domain/search_condition.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 final answersControllerProvider = Provider<Stream<List<Answer>>>((ref) {
-  return ref.watch(listenAnswersUsecaseProvider).listenAnswers();
+  // Todo: Supabaseと繋げる
+  return Stream.periodic(const Duration(seconds: 1), (count) {
+    return List.generate(count, (index) {
+      return Answer(
+          hotItem: HotItem(
+              id: index,
+              title: "title",
+              description: "description",
+              imageUrl: "imageUrl",
+              searchCondition: SearchCondition(latitude: 0, longitude: 0)));
+    });
+  });
+  // return ref.watch(listenAnswersUsecaseProvider).listenAnswers();
 });
