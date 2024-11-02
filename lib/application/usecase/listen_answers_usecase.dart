654094e858db81c4ec9e4a9e8aebfc6bb7346145
miyasic <kou.sepak@gmail.com>
2024-11-02 22:11:20 +0900

    usecase直呼び出しだけど、とりあえずstreamを流し込めた

--- lib/application/usecase/listen_answers_usecase.dart
+++ lib/application/usecase/listen_answers_usecase.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_hackathon_2024/domain/answer.dart';
+import 'package:flutter_hackathon_2024/domain/hot_item.dart';
+import 'package:flutter_hackathon_2024/domain/search_condition.dart';
 import 'package:flutter_hackathon_2024/infrastructure/repository/common_repository.dart';
 
 class ListenAnswersUsecase {
@@ -6,6 +8,18 @@ class ListenAnswersUsecase {
   ListenAnswersUsecase(this._repository);
 
   Stream<List<Answer>> listenAnswers() {
+    return Stream.periodic(const Duration(seconds: 1), (count) {
+      return List.generate(count, (index) {
+        return Answer(
+            id: index,
+            hotItem: HotItem(
+                id: index,
+                title: "title",
+                description: "description",
+                imageUrl: "imageUrl",
+                searchCondition: SearchCondition(latitude: 0, longitude: 0)));
+      });
+    });
     return _repository.fetchAnswerStream();
   }
 }
--- lib/ui/page/game/hot_game.dart
+++ lib/ui/page/game/hot_game.dart
@@ -1,6 +1,10 @@
 import 'package:flame/game.dart';
 import 'package:flame_riverpod/flame_riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hackathon_2024/application/provider/usecase_providers.dart';
+import 'package:flutter_hackathon_2024/ui/controller/answers_controller.dart';
+import 'package:flutter_hackathon_2024/ui/page/game/sample_component.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class HotGame extends FlameGame with RiverpodGameMixin {
   @override
@@ -9,5 +13,11 @@ class HotGame extends FlameGame with RiverpodGameMixin {
   @override
   Future<void> onLoad() async {
     await super.onLoad();
+    add(SampleComponent());
+    final snap = ref.read(listenAnswersUsecaseProvider).listenAnswers();
+    snap.listen((event) {
+      print('event');
+      add(SampleComponent());
+    });
   }
 }
