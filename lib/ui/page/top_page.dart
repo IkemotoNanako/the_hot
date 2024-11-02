426614e26835e4be394023770a3990266c1c7d2c
miyasic <kou.sepak@gmail.com>
2024-11-02 18:42:07 +0900

    GameWidgetを追加

--- lib/ui/page/top_page.dart
+++ lib/ui/page/top_page.dart
@@ -1,3 +1,6 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flame_riverpod/flame_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hackathon_2024/ui/controller/answers_controller.dart';
 import 'package:flutter_hackathon_2024/ui/router/router.dart';
@@ -16,6 +19,7 @@ class TopPage extends ConsumerWidget {
       body: Stack(
         children: [
           const _AnswerList(),
+          const _Game(),
           Center(
               child: OutlinedButton(
                   onPressed: () {
@@ -26,6 +30,23 @@ class TopPage extends ConsumerWidget {
       ),
     );
   }
+}
+
+class _Game extends ConsumerWidget {
+  const _Game();
+
+  @override
+  Widget build(
+    BuildContext context,
+    WidgetRef ref,
+  ) {
+    return GameWidget(game: MyGame());
+  }
+}
+
+class MyGame extends FlameGame {
+  @override
+  Future<void> onLoad() async {}
 }
 
 class _AnswerList extends ConsumerWidget {
