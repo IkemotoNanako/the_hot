411a281009df5ff6815a5d41e2a6eaf5d2a04155
IkemotoNanako <chibencaizi@gmail.com>
2024-11-03 02:04:15 +0900

    refactor: lint警告解消

--- lib/ui/page/top_page.dart
+++ lib/ui/page/top_page.dart
@@ -2,8 +2,6 @@ import 'package:flame_riverpod/flame_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hackathon_2024/domain/enum/button_color_type.dart';
 import 'package:flutter_hackathon_2024/ui/component/custom_circle_button.dart';
-import 'package:flutter_hackathon_2024/ui/component/custom_filled_button.dart';
-import 'package:flutter_hackathon_2024/ui/component/custom_outlined_button.dart';
 import 'package:flutter_hackathon_2024/ui/page/game/hot_game.dart';
 import 'package:flutter_hackathon_2024/ui/router/router.dart';
 import 'package:go_router/go_router.dart';
