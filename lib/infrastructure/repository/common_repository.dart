2ddad10481438b57a86ea1541a2b3b9d63872a0d
miyasic <kou.sepak@gmail.com>
2024-11-02 15:44:59 +0900

    メモリキャッシュを実装

--- lib/infrastructure/repository/common_repository.dart
+++ lib/infrastructure/repository/common_repository.dart
@@ -1,13 +1,21 @@
 import 'package:flutter_hackathon_2024/domain/hot_item.dart';
 import 'package:flutter_hackathon_2024/domain/question.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+@Riverpod(keepAlive: true)
 class CommonRepository {
-  Future<List<HotItem>> fetchHotItemAll() async {
-    return [];
+  List<HotItem>? _hotItemAll;
+  List<Question>? _questionAll;
+
+  List<HotItem> get hotItemAll => _hotItemAll!;
+  List<Question> get questionAll => _questionAll!;
+
+  Future<void> fetchHotItemAll() async {
+    return;
   }
 
-  Future<List<Question>> fetchQuestionAll() async {
-    return [];
+  Future<void> fetchQuestionAll() async {
+    return;
   }
 
   Future<void> setAnswer(HotItem hotItem) async {
@@ -15,6 +23,6 @@ class CommonRepository {
   }
 
   Stream<Question> fetchQuestionStream() {
-    return Stream.empty();
+    return const Stream.empty();
   }
 }
