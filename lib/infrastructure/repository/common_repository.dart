6d367d98c87a220d1b033fa76f389ba74056d05f
yuji4072 <morisima0929@gmail.com>
2024-11-02 17:19:07 +0900

    feat: masterデータをsupabaseからfetch

--- lib/infrastructure/repository/common_repository.dart
+++ lib/infrastructure/repository/common_repository.dart
@@ -1,19 +1,40 @@
 import 'package:flutter_hackathon_2024/domain/hot_item.dart';
 import 'package:flutter_hackathon_2024/domain/question.dart';
+import 'package:flutter_hackathon_2024/util/constant.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class CommonRepository {
+  final supabase = Supabase.instance.client;
+
   List<HotItem>? _hotItemAll;
   List<Question>? _questionAll;
 
   List<HotItem> get hotItemAll => _hotItemAll!;
+
   List<Question> get questionAll => _questionAll!;
 
-  Future<void> fetchHotItemAll() async {
-    return;
+  Future<List<HotItem>> fetchHotItemAll() async {
+    try {
+      final response = await supabase.from(tableNameHotItems).select();
+      return response.map((json) {
+        return HotItem.fromJson(json);
+      }).toList();
+    } catch (e) {
+      // todo: エラーハンドリング
+      rethrow;
+    }
   }
 
-  Future<void> fetchQuestionAll() async {
-    return;
+  Future<List<Question>> fetchQuestionAll() async {
+    try {
+      final response = await supabase.from(tableNameQuestions).select();
+      return response.map((json) {
+        return Question.fromJson(json);
+      }).toList();
+    } catch (e) {
+      // todo: エラーハンドリング
+      rethrow;
+    }
   }
 
   Future<void> setAnswer(HotItem hotItem) async {
