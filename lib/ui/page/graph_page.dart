6b6e3d3977b7b9a67b7fb6a7222d73bf5ea32615
miyasic <kou.sepak@gmail.com>
2024-11-03 10:49:08 +0900

    タイトルを表示

--- lib/ui/page/graph_page.dart
+++ lib/ui/page/graph_page.dart
@@ -1,13 +1,26 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hackathon_2024/ui/style/custom_text_theme.dart';
 
 class GraphPage extends StatelessWidget {
   const GraphPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    // タイトル
+    final titleData = FlTitlesData(
+      topTitles: AxisTitles(
+          axisNameWidget: Text('みんなの結果', style: customTextTheme.headlineLarge),
+          axisNameSize: 100),
+    );
+
+    return Scaffold(
       body: Center(
-        child: Text('Graph Page'),
+        child: BarChart(
+          BarChartData(
+            titlesData: titleData,
+          ),
+        ),
       ),
     );
   }
