c2833e8c756a7bc0141eca2a1e49a490a9f4ebdd
miyasic <kou.sepak@gmail.com>
2024-11-03 11:14:17 +0900

    グラフの見た目を整えました

--- lib/ui/page/graph_page.dart
+++ lib/ui/page/graph_page.dart
@@ -12,13 +12,62 @@ class GraphPage extends StatelessWidget {
       topTitles: AxisTitles(
           axisNameWidget: Text('みんなの結果', style: customTextTheme.headlineLarge),
           axisNameSize: 100),
+      // 右のメモリを消す
+      rightTitles: const AxisTitles(
+        sideTitles: SideTitles(showTitles: false),
+      ),
+      leftTitles: AxisTitles(
+        sideTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 100,
+          interval: 20, // 最大値を5で割るとかが良さそう
+          getTitlesWidget: (value, meta) => SideTitleWidget(
+            axisSide: meta.axisSide,
+            child: Text(
+              value.toString(),
+              style: customTextTheme.headlineLarge?.copyWith(
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
 
+    // 左のメモリの調整
+
+    const barNum = 14;
+
     return Scaffold(
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: BarChart(
           BarChartData(
             titlesData: titleData,
+            // グリッドを消す
+            gridData: const FlGridData(show: false),
+            // 枠線を上下左右だけ消す
+            borderData: FlBorderData(
+              show: true,
+              border: const Border(
+                top: BorderSide.none,
+                right: BorderSide.none,
+                left: BorderSide.none,
+                bottom: BorderSide(color: Colors.black, width: 1),
+              ),
+            ),
+
+            barGroups: [
+              for (int i = 0; i < barNum; i++)
+                BarChartGroupData(x: 1, barRods: [
+                  BarChartRodData(
+                      fromY: 0,
+                      toY: i.toDouble() * i.toDouble(),
+                      width: 30,
+                      color: Colors.amber,
+                      borderRadius: const BorderRadius.all(Radius.zero)),
+                ]),
+            ],
           ),
         ),
       ),
